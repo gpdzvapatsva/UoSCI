42040fe09de8472a0d9ac474d77bd800783920e3
gpdzvapatsva <gpdzvapatsva@gmail.com>
2024-10-11 13:31:33 +0100

    some pull rq

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+from myapp.models import Students
+
+
 # Create your views here.
 def index(request):
     return render (request, 'index.html')
 def records(request):
+    students=Students
+    context={'students':students}
     return render (request, 'records.html')
 def contact(request):
     return render(request, template_name='contact.html')
