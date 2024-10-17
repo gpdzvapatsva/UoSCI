8ea6cc059d33428603cd99105b46f974f5a55fca
gpdzvapatsva <gpdzvapatsva@gmail.com>
2024-10-17 21:46:32 +0100

    changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 path('records/', views.records, name='records'),
-
+path('contacts/', views.records, name='contacts'),
 ]
